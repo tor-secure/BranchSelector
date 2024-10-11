041f75ba87cfa67fc612d5e12be22112715e7a0e
Anish U <anish98821@hotmail.com>
2024-10-11 23:24:41 +0530

    changed buy credit section in dashboard

--- src/Components/Dashboard/BuyCreditsSection.jsx
+++ src/Components/Dashboard/BuyCreditsSection.jsx
@@ -1,19 +1,57 @@
+import { useEffect, useState } from "react";
 import { FaArrowRight } from "react-icons/fa"
+import { useNavigate } from "react-router-dom";
+import { getPricingPlans } from "../../pages/PricingPage/PricingPlans";
+import { LoadingPage } from "../../pages/LoadingPage";
+import { LargeContainerCard, SmallCard } from "../../pages/PricingPage/PricingPage";
+
 
 //Section in user dashboard
 export const BuyCreditsSection = () => {
+  const navigate = useNavigate();
+
+  const handleCheckout = (plan,currencyCode) =>{
+  navigate('/checkout',{state:{plan:plan,currencyCode:currencyCode}})
+  }
+ 
+  const [plans, setPlans] = useState(null);
+  const [currency, setCurrency] = useState('₹');
+  const [loading, setLoading] = useState(true);
+  const [currencyCode, setCurrencyCode] = useState(null)
+
+  useEffect(() => {
+    const fetchPricingPlans = async () => {
+      const data = await getPricingPlans();
+      if (data) {
+
+        setPlans(data.plan);
+        setCurrency(data.currency);
+        setCurrencyCode(data.currencyCode);
+      }
+      setLoading(false);
+    };
+
+    fetchPricingPlans();
+  }, []);
+  if (loading) {
+    return <LoadingPage/>;
+  }
+  
+  const CreditPlanCard = ({plans,currency,currencyCode,clickHandler}) =>{
+    return(
+          <LargeContainerCard title="Credit Bundle">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 gap-4">
+              {plans.credits.map((plan, index) => (
+                <SmallCard key={index} plan={plan} currency={currency} currencyCode={currencyCode} clickHandler={clickHandler}/>
+              ))}
+            </div>
+          </LargeContainerCard>
+    )
+  }
+
   return (
     <div className="bg-white p-5 my-8 mt-0 md:w-max max-w-md mx-auto">
-      <div className="px-5 border shadow-sm rounded-lg text-lg font-bold py-10">
-
-        Checkout our pricing page for amazing bundles!
-
-        <div className=" flex self-center mt-10" href='/pricing'>
-            <FaArrowRight color="367af3" className="mr-2 mt-1"/>
-            <a href='/pricing' className="text-lg font-bold text-[#367af3]">Go to Pricing Page</a>
-        </div>
-
-      </div>
+      <CreditPlanCard plans={plans} currency = {currency} currencyCode={currencyCode} clickHandler={handleCheckout}/>
       
     </div>
   );
